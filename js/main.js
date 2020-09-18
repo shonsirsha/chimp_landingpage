adc69e71f0ac3af75a9d21faa52310ec4910ad78
sean <seangeekpro@gmail.com>
2020-09-18 15:36:27 +0700

    switches

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-changeFeatureTitle();
+let images = [
+  ["./img/gif0.gif", "./img/gif1.gif"],
+  ["./img/gif2.gif", "./img/gif3.gif"],
+  ["./img/gif0.gif"],
+];
 
-$(window).resize(function () {
-  changeFeatureTitle();
+for (let i = 0; i < images.length; i++) {
+  for (let x = 0; x < images[i].length; x++) {
+    $(`.preview-${i}`).attr("src", images[i][0]);
+  }
+}
+
+$(".switch").on("click", function () {
+  $(this).find(".switch-btn").toggleClass("active");
+  let num = getSecondPart($(this).attr("id"));
+  if ($(this).find(".switch-btn").hasClass("active")) {
+    $(`.preview-${num}`).attr("src", images[num][1]);
+  } else {
+    $(`.preview-${num}`).attr("src", images[num][0]);
+  }
 });
 
-function changeFeatureTitle() {
-  if (window.innerWidth <= 512) {
-    $(".feature-title").html("Current Feature:");
-  } else {
-    $(".feature-title").html("Features:");
-  }
+function getSecondPart(str) {
+  return str.split("-")[1];
 }
