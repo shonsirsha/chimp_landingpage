6bb5b9cc9c6a264b095d53bc2c530bf39f797c45
sean <seangeekpro@gmail.com>
2020-09-18 15:57:54 +0700

    .

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
-let images = [
-  ["./img/gif0.gif", "./img/gif1.gif"],
-  ["./img/gif2.gif", "./img/gif3.gif"],
-  ["./img/gif0.gif"],
-];
+// let images = [
+//   ["./img/gif0.gif", "./img/gif1.gif"],
+//   ["./img/gif2.gif", "./img/gif3.gif"],
+//   ["./img/gif0.gif"],
+// ];
 
-for (let i = 0; i < images.length; i++) {
-  for (let x = 0; x < images[i].length; x++) {
-    $(`.preview-${i}`).attr("src", images[i][0]);
-  }
-}
+// for (let i = 0; i < images.length; i++) {
+//   for (let x = 0; x < images[i].length; x++) {
+//     $(`.preview-${i}`).attr("src", images[i][0]);
+//   }
+// }
 
 $(".switch").on("click", function () {
   $(this).find(".switch-btn").toggleClass("active");
   let num = getSecondPart($(this).attr("id"));
+
   if ($(this).find(".switch-btn").hasClass("active")) {
-    $(`.preview-${num}`).attr("src", images[num][1]);
+    $(`.preview-${num}.img-0`).addClass("hide");
+    $(`.preview-${num}.img-1`).removeClass("hide");
   } else {
-    $(`.preview-${num}`).attr("src", images[num][0]);
+    $(`.preview-${num}.img-0`).removeClass("hide");
+    $(`.preview-${num}.img-1`).addClass("hide");
   }
 });
+
+// $(".switch").on("click", function () {
+//     $(this).find(".switch-btn").toggleClass("active");
+//     let num = getSecondPart($(this).attr("id"));
+//     if ($(this).find(".switch-btn").hasClass("active")) {
+//       $(`.preview-${num}`).attr("src", images[num][1]);
+//     } else {
+//       $(`.preview-${num}`).attr("src", images[num][0]);
+//     }
+//   });
 
 function getSecondPart(str) {
   return str.split("-")[1];
